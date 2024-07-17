6947a12ea4e9d2d99545b4d2c607d5f4c5743f77
nidhinakranii <80626423+nidhinakranii@users.noreply.github.com>
2024-07-17 00:13:53 -0400

    Update contracts-config.js

--- contracts-config.js
+++ contracts-config.js
@@ -1,5 +1,5 @@
 
-    export const stakingContractAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
+    export const stakingContractAddress = "0xf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
     export const nftContractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
     export const tokenContractAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
     export const ownerAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
